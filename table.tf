4e26db90223a63d8b8aaafb3e0c7cd191325b0b3
Kelvinskell <kelvinskelll@gmail.com>
2023-12-18 05:53:48 +0100

    fmt

--- table.tf
+++ table.tf
@@ -1,9 +1,9 @@
 # Create DynamoDB table
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "orders"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "orderId"
-  stream_enabled = true
+  name             = "orders"
+  billing_mode     = "PAY_PER_REQUEST"
+  hash_key         = "orderId"
+  stream_enabled   = true
   stream_view_type = "NEW_IMAGE"
 
   attribute {
