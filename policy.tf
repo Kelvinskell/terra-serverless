a8687183127d4c07ccb9cb214ed0ca0eea02a4f4
Kelvinskell <kelvinskelll@gmail.com>
2023-12-18 03:48:37 +0100

    terraform fmt

--- policy.tf
+++ policy.tf
@@ -1,8 +1,8 @@
 # Create IAM Policies
 
 resource "aws_iam_policy" "lambda_dynamo" {
-  name = "Lambda-Write-DynamoDB"
-  path = "/"
+  name        = "Lambda-Write-DynamoDB"
+  path        = "/"
   description = "A policy for lambda to put items into DunamoDB"
 
   policy = jsonencode({
@@ -22,69 +22,69 @@ resource "aws_iam_policy" "lambda_dynamo" {
 }
 
 resource "aws_iam_policy" "lambda_sns" {
-    name = "Lambda-SNS-Publish"
-    path = "/"
-    description = "A policy for Amazon SNS to get, list, and publish topics that are received by Lambda"
+  name        = "Lambda-SNS-Publish"
+  path        = "/"
+  description = "A policy for Amazon SNS to get, list, and publish topics that are received by Lambda"
 
-    policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "sns:Publish",
-                "sns:GetTopicAttributes",
-                    "sns:ListTopics"
-            ],
-                "Resource": "*"
-        }
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "sns:Publish",
+          "sns:GetTopicAttributes",
+          "sns:ListTopics"
+        ],
+        "Resource" : "*"
+      }
     ]
- })
+  })
 }
 
 resource "aws_iam_policy" "lambda_dynamostreams" {
-    name = "Lambda-DynamoDBStreams-Read"
-    path = "/"
-    description = "A policy for Lambda to get records from DynamoDB Streams"
+  name        = "Lambda-DynamoDBStreams-Read"
+  path        = "/"
+  description = "A policy for Lambda to get records from DynamoDB Streams"
 
-    policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:GetShardIterator",
-                "dynamodb:DescribeStream",
-                "dynamodb:ListStreams",
-                "dynamodb:GetRecords"
-            ],
-            "Resource": "*"
-        }
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:GetShardIterator",
+          "dynamodb:DescribeStream",
+          "dynamodb:ListStreams",
+          "dynamodb:GetRecords"
+        ],
+        "Resource" : "*"
+      }
     ]
-})
+  })
 }
 
 resource "aws_iam_policy" "lambda_sqs" {
-    name = "Lambda-Read-SQS"
-    path = "/"
-    description = "A policy for Lambda to read messages that are placed in Amazon SQS"
+  name        = "Lambda-Read-SQS"
+  path        = "/"
+  description = "A policy for Lambda to read messages that are placed in Amazon SQS"
 
-    policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "sqs:DeleteMessage",
-                "sqs:ReceiveMessage",
-                "sqs:GetQueueAttributes",
-                "sqs:ChangeMessageVisibility"
-            ],
-            "Resource": "*"
-        }
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "sqs:DeleteMessage",
+          "sqs:ReceiveMessage",
+          "sqs:GetQueueAttributes",
+          "sqs:ChangeMessageVisibility"
+        ],
+        "Resource" : "*"
+      }
     ]
-})
+  })
 }
