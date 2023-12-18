dce4dbe8c1a9430baef3a29c62eef231f8fb261f
Kelvinskell <kelvinskelll@gmail.com>
2023-12-18 15:22:20 +0100

    Update lambda config

--- lambda.tf
+++ lambda.tf
@@ -1,6 +1,12 @@
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambda_function_payload.zip"
+resource "aws_lambda_function" "python_lambda" {
+  filename      = "main.py.zip"
   function_name = "POC_Lambda-1"
   role          = aws_iam_role.role1.arn
   runtime = "python3.9"
+  handler = "main.py.zip"
+}
+
+resource "aws_lambda_event_source_mapping" "example" {
+  event_source_arn = aws_sqs_queue.standard_queue.arn
+  function_name    = aws_lambda_function.python_lambda.arn
 }
