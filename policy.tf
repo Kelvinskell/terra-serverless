334695b2f9d2e11816f7919215db9feeadcaefd9
Kelvinskell <kelvinskelll@gmail.com>
2023-12-18 03:43:04 +0100

    update iam policies

--- policy.tf
+++ policy.tf
@@ -1,10 +1,10 @@
 # Create IAM Policies
-resource "aws_iam_policy" "policy" {
+
+resource "aws_iam_policy" "lambda_dynamo" {
   name = "Lambda-Write-DynamoDB"
   path = "/"
+  description = "A policy for lambda to put items into DunamoDB"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -19,4 +19,72 @@ resource "aws_iam_policy" "policy" {
       }
     ]
   })
+}
+
+resource "aws_iam_policy" "lambda_sns" {
+    name = "Lambda-SNS-Publish"
+    path = "/"
+    description = "A policy for Amazon SNS to get, list, and publish topics that are received by Lambda"
+
+    policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "sns:Publish",
+                "sns:GetTopicAttributes",
+                    "sns:ListTopics"
+            ],
+                "Resource": "*"
+        }
+    ]
+ })
+}
+
+resource "aws_iam_policy" "lambda_dynamostreams" {
+    name = "Lambda-DynamoDBStreams-Read"
+    path = "/"
+    description = "A policy for Lambda to get records from DynamoDB Streams"
+
+    policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:GetShardIterator",
+                "dynamodb:DescribeStream",
+                "dynamodb:ListStreams",
+                "dynamodb:GetRecords"
+            ],
+            "Resource": "*"
+        }
+    ]
+})
+}
+
+resource "aws_iam_policy" "lambda_sqs" {
+    name = "Lambda-Read-SQS"
+    path = "/"
+    description = "A policy for Lambda to read messages that are placed in Amazon SQS"
+
+    policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "sqs:DeleteMessage",
+                "sqs:ReceiveMessage",
+                "sqs:GetQueueAttributes",
+                "sqs:ChangeMessageVisibility"
+            ],
+            "Resource": "*"
+        }
+    ]
+})
 }
