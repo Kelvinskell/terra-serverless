ebf70be33fe27a5856a6be2cd6d74848827780aa
Kelvinskell <kelvinskelll@gmail.com>
2023-12-19 11:13:37 +0100

    configure second lamda function

--- lambda.tf
+++ lambda.tf
@@ -7,7 +7,17 @@ resource "aws_lambda_function" "python_lambda" {
   handler = "main.lambda_handler"
 }
 
+resource "aws_lambda_function" "python_lambda2" {
+  filename      = "main2.py.zip"
+  function_name = "POC_Lambda-2"
+  role          = aws_iam_role.role2.arn
+  source_code_hash = filebase64sha256("main2.py.zip")
+  runtime = "python3.9"
+  handler = "main.lambda_handler"
+}
+
 resource "aws_lambda_event_source_mapping" "example" {
   event_source_arn = aws_sqs_queue.standard_queue.arn
   function_name    = aws_lambda_function.python_lambda.arn
 }
+
