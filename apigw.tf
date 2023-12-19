0af090283a6a82c1f9a291a61fef89897f39bd44
Kelvinskell <kelvinskelll@gmail.com>
2023-12-19 16:59:18 +0100

    Update config for REST API GW

--- apigw.tf
+++ apigw.tf
@@ -30,6 +30,33 @@ resource "aws_api_gateway_integration" "apigw_integration" {
   }
 }
 
+# Construct method response
+resource "aws_api_gateway_method_response" "success_response" {
+  rest_api_id = aws_api_gateway_rest_api.apigw.id
+  resource_id = aws_api_gateway_rest_api.apigw.root_resource_id
+  http_method = aws_api_gateway_method.apigw_method.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+# construct integration response
+resource "aws_api_gateway_integration_response" "success_response" {
+  rest_api_id       = aws_api_gateway_rest_api.apigw.id
+  resource_id       = aws_api_gateway_rest_api.apigw.root_resource_id
+  http_method       = aws_api_gateway_method.apigw_method.http_method
+  status_code       = aws_api_gateway_method_response.success_response.status_code
+  selection_pattern = "^2[0-9][0-9]" // regex pattern for any 200 message that comes back from SQS
+
+  response_templates = {
+    "application/json" = "{\"message\": \"great success!\"}"
+  }
+
+  depends_on = [aws_api_gateway_integration.apigw_integration]
+}
+
 # Deploy API
 resource "aws_api_gateway_deployment" "apigw_deploy" {
   rest_api_id = aws_api_gateway_rest_api.apigw.id
